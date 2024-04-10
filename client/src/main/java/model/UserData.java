99d415d10c9158c41c1376ff344cfa89034523a6
TeaBit76 <alexander.delsanto@edu.unito.it>
2024-04-10 21:33:56 +0200

    adding UserData.java

---
+++ client/src/main/java/model/UserData.java
@@ -0,0 +1,33 @@
+package model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class UserData {
+
+    private static UserData instance = null;
+    public static synchronized UserData getInstance(){
+        if(instance == null){
+            instance = new UserData();
+            instance.setName("");
+            instance.setStatus("");
+        }
+
+        return instance;
+    }
+    private UserData(){}
+
+
+    private final StringProperty name = new SimpleStringProperty("");
+    private final StringProperty status = new SimpleStringProperty("");
+
+    public StringProperty userProperty() {
+        return name;
+    }
+
+    public StringProperty statusProperty() { return status; }
+
+    public void setName(String user) { this.name.set(user); }
+
+    public void setStatus(String status) { this.status.set(status); }
+}
