e35aeda45c495588e159549e93cba28ffec4dd89
TeaBit76 <alexander.delsanto@edu.unito.it>
2024-04-10 21:31:58 +0200

    changes to StatusBarController

---
+++ client/src/main/java/frontend/component/StatusBarController.java
@@ -0,0 +1,25 @@
+package frontend.component;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import model.UserData;
+
+public class StatusBarController {
+    @FXML private Label userLabel;
+    @FXML private Label statusLabel;
+    private final UserData userData = UserData.getInstance();
+
+    @FXML
+    private void initialize() {
+        userLabel.textProperty().bind(userData.userProperty());
+        statusLabel.textProperty().bind(userData.statusProperty());
+    }
+
+    public void setUser(String name) {
+        userData.setName(name);
+    }
+
+    public void setStatus(String status) {
+        userData.setStatus(status);
+    }
+}
